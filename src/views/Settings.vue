83bd4652b4946a41a38607f7ca72f1dca7670b6c
shashwatbangar <shashwatbangar@gmail.com>
2022-12-07 11:27:20 +0530

    Implemented: Updated UI of Settings page(#32j3r6t)

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -7,38 +7,70 @@
     </ion-header>
     
     <ion-content>
-      <ion-list>
-        <!-- Select store -->
-        <ion-item>
-          <ion-icon :icon="storefrontOutline" slot="start" />
-          <ion-label>{{$t("eCom Store")}}</ion-label>
-          <ion-select interface="popover" :value="currentFacility.facilityId" @ionChange="setFacility($event)">
-            <ion-select-option v-for="facility in (userProfile ? userProfile.facilities : [])" :key="facility.facilityId" :value="facility.facilityId" >{{ facility.name }}</ion-select-option>
-          </ion-select>
-        </ion-item>
+      <div class="user-profile">
+        <ion-card>
+          <ion-item lines="full">
+            <ion-avatar slot="start" v-if="userProfile?.partyImageUrl">
+              <Image :src="userProfile.partyImageUrl"/>
+            </ion-avatar>
+            <ion-label>
+              {{ userProfile?.partyName }}
+              <p>{{ userProfile?.userLoginId }}</p>
+            </ion-label>
+          </ion-item>
+          <ion-button fill="outline" color="danger" @click="logout()">{{ $t("Logout") }}</ion-button>
+          <!-- Commenting this code as we currently do not have reset password functionality -->
+          <!-- <ion-button fill="outline" color="medium">{{ $t("Reset password") }}</ion-button> -->
+        </ion-card>
+      </div>
+      <h1>{{ $t('OMS') }}</h1>
+      <section>
+        <ion-card>
+          <ion-card-header>
+            <ion-card-subtitle>
+              {{ $t("OMS instance") }}
+            </ion-card-subtitle>
+            <ion-card-title>
+              {{ instanceUrl }}
+            </ion-card-title>
+          </ion-card-header>
+          <ion-card-content>
+            {{ $t('This is the name of the OMS you are connected to right now. Make sure that you are connected to the right instance before proceeding.') }}
+          </ion-card-content>
+          <ion-button @click="goToOms" fill="clear">
+            {{ $t('Go to OMS') }}
+            <ion-icon slot="end" :icon="openOutline" />
+          </ion-button>
+        </ion-card>
 
-        <!-- OMS information -->
-        <ion-item>
-          <ion-icon :icon="codeWorkingOutline" slot="start"/>
-          <ion-label>{{ $t("OMS") }}</ion-label>
-          <ion-label slot="end">{{ baseURL ? baseURL : instanceUrl }}</ion-label>
-        </ion-item>
-
-        <!-- Profile of user logged in -->
-        <ion-item>
-          <ion-icon :icon="personCircleOutline" slot="start" />
-          <ion-label>{{ userProfile !== null ? userProfile.partyName : '' }}</ion-label>
-         <ion-button slot="end" fill="outline" color="dark" @click="logout()">{{ $t("Logout") }}</ion-button>
-        </ion-item>
-      </ion-list>
+        <ion-card>
+          <ion-card-header>
+            <ion-card-subtitle>
+              {{ $t("Shopify Config") }}
+            </ion-card-subtitle>
+            <ion-card-title>
+              {{ $t("eCommerce") }}
+            </ion-card-title>
+          </ion-card-header>
+          <ion-card-content>
+            {{ $t('eCommerce stores are directly connected to one Shop Configs. If your OMS is connected to multiple eCommerce stores selling the same catalog operating as one Company, you may have multiple Shop Configs for the selected Product Store.') }}
+          </ion-card-content>
+          <ion-item lines="none">
+            <ion-label>{{ $t("Select eCommerce") }}</ion-label>
+            <ion-select interface="popover" :value="currentFacility.facilityId" @ionChange="setFacility($event)">
+              <ion-select-option v-for="facility in (userProfile ? userProfile.facilities : [])" :key="facility.facilityId" :value="facility.facilityId" >{{ facility.name }}</ion-select-option>
+            </ion-select>
+          </ion-item>
+        </ion-card>
+      </section>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { alertController, IonButton, IonContent, IonHeader,IonIcon, IonItem, IonLabel, IonPage, IonSelect, IonSelectOption, IonTitle, IonToolbar, IonList } from '@ionic/vue';
+import { alertController, IonButton, IonContent, IonHeader,IonIcon, IonItem, IonLabel, IonPage, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/vue';
 import { defineComponent } from 'vue';
-import { codeWorkingOutline, ellipsisVertical, personCircleOutline, storefrontOutline} from 'ionicons/icons'
+import { codeWorkingOutline, ellipsisVertical, personCircleOutline, storefrontOutline, openOutline} from 'ionicons/icons'
 import { mapGetters, useStore } from 'vuex';
 import { useRouter } from 'vue-router';
 
@@ -56,7 +88,6 @@ export default defineComponent({
     IonSelectOption,
     IonTitle, 
     IonToolbar,
-    IonList
   },
   data() {
     return {
@@ -105,6 +136,9 @@ export default defineComponent({
           this.router.push('/login');
         })
       }
+    },
+    goToOms(){
+      window.location.href = this.instanceUrl.startsWith('http') ? this.instanceUrl.replace('api/', "") : `https://${this.instanceUrl}.hotwax.io/`;
     }
   },
   setup(){
@@ -116,6 +150,7 @@ export default defineComponent({
       ellipsisVertical,
       personCircleOutline,
       storefrontOutline,
+      openOutline,
       store,
       router
     }
@@ -126,4 +161,20 @@ export default defineComponent({
 ion-label[slot="end"] {
   text-align: end;
 }
+
+  ion-card > ion-button {
+    margin: var(--spacer-xs);
+  }
+  h1 {
+    padding: var(--spacer-xs) 10px 0;
+  }
+  section {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    align-items: start;
+  }
+  .user-profile {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+  }
 </style>
