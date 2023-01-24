eafc45e800e1f3318e2457696c2ee4e9ac76472d
k2maan <ketuman333@gmail.com>
2023-01-24 18:40:39 +0530

    Removed console.log (#85zrjnfpj)

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -102,7 +102,6 @@ export default defineComponent({
           // Ionic modal returns data and role where the 'role' string defines the element that is dismissing the modal (backdrop in our case). 
           // When the modal is dismissed, the role string is set as undefined else it is assigned that element only (backdrop in our case).
           // So, the modal isn't getting dismissed correctly amd that is why result.role returns backdrop
-          console.log(result)
           if(result.role && result.role !== 'backdrop'){
             this.queryString = result.role
             this.getProducts(process.env.VUE_APP_VIEW_SIZE, 0);
