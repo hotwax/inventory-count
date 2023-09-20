35b2964845240a4c9532fdadda0d126816fcf819
Tanishque <tanishque.sharma@gmail.com>
2023-09-20 12:44:50 +0530

    Improved: naming of variables (#201)

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -9,8 +9,8 @@
       <ion-searchbar @ionFocus="selectSearchBarText($event)" v-model="queryString" :placeholder="$t('Search')" @keyup.enter="queryString = $event.target.value; searchProducts()"/>
       
       <!-- Empty state -->
-      <div class="empty-state"  v-if="!products.length && !productFetch">
-        <p v-if="noResultMessage">{{ $t("No results found")}}</p>
+      <div class="empty-state"  v-if="!products.length && !fetchingReturns">
+        <p v-if="showErrorMessage">{{ $t("No results found")}}</p>
         <img src="../assets/images/empty-state.png" alt="No results found"/>
         <p>{{ $t("Enter a SKU, or use the barcode scanner to search a product")}}</p>
       </div>
@@ -89,8 +89,8 @@ export default defineComponent({
   data (){
     return {
       queryString: '',
-      noResultMessage: false,
-      productFetch: false
+      showErrorMessage: false,
+      fetchingReturns: false
     }
   },
   computed: {
@@ -139,8 +139,8 @@ export default defineComponent({
       })
     },
     async searchProducts(vSize?: any, vIndex?: any) {
-      this.queryString ? this.noResultMessage = true: this.noResultMessage = false;
-      this.productFetch = true;
+      this.queryString ? this.showErrorMessage = true: this.showErrorMessage = false;
+      this.fetchingReturns = true;
       const viewSize = vSize ? vSize : process.env.VUE_APP_VIEW_SIZE;
       const viewIndex = vIndex ? vIndex : 0;
       const queryString = '*' + this.queryString + '*';
@@ -154,10 +154,10 @@ export default defineComponent({
       }
       if (this.queryString) {
         await this.store.dispatch("product/findProduct", payload);
-        this.productFetch = false;
+        this.fetchingReturns = false;
       } else {
-        this.productFetch = false;
-        this.noResultMessage = true;
+        this.fetchingReturns = false;
+        this.showErrorMessage = true;
         showToast(translate("Enter product sku to search"))
       }
     }
