b547c821c9c6dc7b4b424b0f22a47976b2309e0b
amansinghbais <singh11amanbais@gmail.com>
2023-09-27 11:26:58 +0530

    Implemented: centralized oms instance navigator in settings page from dxp-components (dxp/177)

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -34,23 +34,8 @@
         <h1>{{ $t('OMS') }}</h1>
       </div>
       <section>
-        <ion-card>
-          <ion-card-header>
-            <ion-card-subtitle>
-              {{ $t("OMS instance") }}
-            </ion-card-subtitle>
-            <ion-card-title>
-              {{ baseURL ? baseURL : instanceUrl }}
-            </ion-card-title>
-          </ion-card-header>
-          <ion-card-content>
-            {{ $t('This is the name of the OMS you are connected to right now. Make sure that you are connected to the right instance before proceeding.') }}
-          </ion-card-content>
-          <ion-button @click="goToOms" fill="clear">
-            {{ $t('Go to OMS') }}
-            <ion-icon slot="end" :icon="openOutline" />
-          </ion-button>
-        </ion-card>
+        <OmsInstanceNavigator />
+
         <ion-card>
           <ion-card-header>
             <ion-card-title>
@@ -279,9 +264,6 @@ export default defineComponent({
     },
     goToLaunchpad() {
       window.location.href = `${process.env.VUE_APP_LOGIN_URL}`
-    },
-    goToOms(){
-      window.open(this.instanceUrl.startsWith('http') ? this.instanceUrl.replace('api/', "") : `https://${this.instanceUrl}.hotwax.io/`, '_blank', 'noopener, noreferrer');
     },
     getDateTime(time: any) {
       return DateTime.fromMillis(time).toLocaleString(DateTime.DATETIME_MED);
