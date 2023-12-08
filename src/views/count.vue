0b52a6d1ec727931b4b6a75940c0f5744d4f2759
shanmukhdutt <ksssaidutt@gmail.com>
2023-12-08 18:12:55 +0530

    Revert "Fixed: Prevented Updating Negative Inventory Counts(#207)"
    This reverts commit e7bcf42cbd8db05e8424fa4634602003bb9abb2c.

--- src/views/count.vue
+++ src/views/count.vue
@@ -216,7 +216,7 @@
         await picker.present();
       },
       updateProductInventoryCount() {
-        if (this.quantity > 0) {
+        if (this.quantity) {
           this.product.quantity = this.quantity;
           this.product.availableQOH = this.availableQOH;
           this.store.dispatch('product/updateInventoryCount', { ...this.product, locationId: this.product.locationId });
@@ -228,9 +228,7 @@
           }
         }])
         this.router.push('/search')
-        } else if(this.quantity < 0) {
-          showToast(translate("Negative stock count cannot be accepted"))
-        }else{
+        } else {
           showToast(translate("Enter the stock count for the product"))
         }
       },
