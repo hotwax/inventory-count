34c2833e9e657d71d30fd39b61a6e35a1a7ae4b5
amansinghbais <singh11amanbais@gmail.com>
2023-12-08 19:08:13 +0530

    Fixed: build failure of app on node version 20

--- tests/unit/example.spec.ts
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Home from '@/views/Home.vue'
-
-describe('Home.vue', () => {
-  it('renders home vue', () => {
-    const wrapper = mount(Home)
-    expect(wrapper.text()).toMatch('Ready to create an app?')
-  })
-})
