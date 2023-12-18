fa84b66d5a98b79aa853cc686438e72fa063acd9
Yash Maheshwari <yashmaheshwari551@gmail.com>
2023-12-18 17:55:35 +0530

    Implemented: support to disable QOH setting when productStore is not found for a facility(#232)

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -84,10 +84,11 @@
           </ion-card-header>
           <ion-card-content>
             {{ $t('Show the current physical quantity expected at locations while counting to help gauge inventory accuracy.') }}
+            <p>{{ $t('Facility needs to be associated with a product store to change this configuration.') }}</p>
           </ion-card-content>
           <ion-item lines="none">
             <ion-label> {{ $t('Show systemic inventory') }} </ion-label>
-            <ion-toggle :disabled="!hasPermission(Actions.APP_QOH_STNG_UPDATE) || Object.keys(currentQOHViewConfig).length == 0" :checked="currentQOHViewConfig.settingValue" @ionChange="updateViewQOHConfig(currentQOHViewConfig, $event.detail.checked)" slot="end" />
+            <ion-toggle :disabled="!hasPermission(Actions.APP_QOH_STNG_UPDATE) || !currentEComStore?.productStoreId || Object.keys(currentQOHViewConfig).length == 0" :checked="currentQOHViewConfig.settingValue" @ionChange="updateViewQOHConfig(currentQOHViewConfig, $event.detail.checked)" slot="end" />
           </ion-item>
         </ion-card>
       </section>
@@ -123,7 +124,7 @@ export default defineComponent({
     IonHeader, 
     IonIcon,
     IonItem, 
-    IonLabel, 
+    IonLabel,
     IonPage, 
     IonSelect,
     IonSelectOption,
