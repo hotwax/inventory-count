5e1139caf22a68c66c50bd6209e24a0a5dd677c1
Yash Maheshwari <yashmaheshwari551@gmail.com>
2023-12-18 16:54:51 +0530

    Implemented: permissionId for accessing the app(#232)

--- src/store/modules/user/actions.ts
+++ src/store/modules/user/actions.ts
@@ -43,9 +43,9 @@ const actions: ActionTree<UserState, RootState> = {
       if (permissionId) {
         // As the token is not yet set in the state passing token headers explicitly
         // TODO Abstract this out, how token is handled should be part of the method not the callee
-        const hasPermission = appPermissions.some((appPermissionId: any) => appPermissionId === permissionId );
+        const hasPermission = appPermissions.some((appPermission: any) => appPermission.action === permissionId );
         // If there are any errors or permission check fails do not allow user to login
-        if (hasPermission) {
+        if (!hasPermission) {
           const permissionError = 'You do not have permission to access the app.';
           showToast(translate(permissionError));
           console.error("error", permissionError);
