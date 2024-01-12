812d7814b8facba4a526c8c53e4a67aea7dc74f5
amansinghbais <singh11amanbais@gmail.com>
2024-01-12 19:02:06 +0530

    Fixed: solr query for fetching products on count page

--- src/store/modules/product/actions.ts
+++ src/store/modules/product/actions.ts
@@ -28,7 +28,6 @@ const actions: ActionTree<ProductState, RootState> = {
       }
       const productQueryPayload = prepareProductQuery(params)
       resp = await ProductService.fetchProducts(productQueryPayload)
-
       // resp.data.response.numFound tells the number of items in the response
       if (resp.status === 200 && resp.data.response?.numFound > 0 && !hasError(resp)) {
         let products = resp.data.response.docs;
@@ -97,10 +96,17 @@ const actions: ActionTree<ProductState, RootState> = {
     if(currentProduct) {
       commit(types.PRODUCT_CURRENT_UPDATED, { product: currentProduct })
     } else {
-      const resp = await ProductService.fetchProducts({
-        // used sku as we are currently only using sku to search for the product
-        "filters": ['sku: ' + payload, 'isVirtual: false'],
-      })
+      // used sku as we are currently only using sku to search for the product
+      const params = {
+        filters: {
+          isVirtual: { value: 'false' },
+          sku: { value: payload }
+        }
+      }
+      const productQueryPayload = prepareProductQuery(params)
+
+      const resp = await ProductService.fetchProducts(productQueryPayload)
+
       if(resp.status === 200 && !hasError(resp) && resp.data.response?.numFound > 0) {
         commit(types.PRODUCT_CURRENT_UPDATED, { product: resp.data.response.docs[0] })
       } else {
