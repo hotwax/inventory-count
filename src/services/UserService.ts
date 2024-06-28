2b7abd360f45ad6a703b705cc7a72e84e2508bf1
Yash Maheshwari <yashmaheshwari551@gmail.com>
2024-06-28 14:43:19 +0530

    Fixed: issue of request url is not formed correctly when feching permissions(#312)

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -85,10 +85,9 @@ const setUserTimeZone = async (payload: any): Promise <any>  => {
   });
 }
 
-const getUserPermissions = async (payload: any, token: any): Promise<any> => {
+const getUserPermissions = async (payload: any, url: string, token: any): Promise<any> => {
   // Currently, making this request in ofbiz
-  const omsRedirectionInfo = store.getters["user/getOmsRedirectionInfo"]
-  const baseURL = omsRedirectionInfo.url.startsWith('http') ? omsRedirectionInfo.url.includes('/api') ? omsRedirectionInfo.url : `${omsRedirectionInfo.url}/api/` : `https://${omsRedirectionInfo.url}.hotwax.io/api/`;
+  const baseURL = url.startsWith('http') ? url.includes('/api') ? url : `${url}/api/` : `https://${url}.hotwax.io/api/`;
   let serverPermissions = [] as any;
 
   // If the server specific permission list doesn't exist, getting server permissions will be of no use
