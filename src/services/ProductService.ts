dd29314ac6ab04bda71d78bf882ea9856252948e
Yash Maheshwari <yashmaheshwari551@gmail.com>
2024-07-01 12:21:31 +0530

    Improved: productService to use client method for making the api call

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,12 +1,20 @@
-import api from '@/api';
+import { client } from '@/api';
+import store from '@/store';
 
 const fetchProducts = async (query: any): Promise <any>  => {
-  return api({
+  const omsRedirectionInfo = store.getters["user/getOmsRedirectionInfo"]
+  const baseURL = omsRedirectionInfo.url.startsWith('http') ? omsRedirectionInfo.url.includes('/api') ? omsRedirectionInfo.url : `${omsRedirectionInfo.url}/api/` : `https://${omsRedirectionInfo.url}.hotwax.io/api/`;
+
+  console.log(omsRedirectionInfo)
+  return await client({
     url: "searchProducts",
-    method: "post",
+    method: "POST",
+    baseURL,
     data: query,
-    cache: true,
-    useOmsRedirection: true
+    headers: {
+      "Authorization":  'Bearer ' + omsRedirectionInfo.token,
+      'Content-Type': 'application/json'
+    }
   });
 }
 
