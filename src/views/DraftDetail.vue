dd27be50d02e861ef461fbca98aacf4e215fd0d5
R-Sourabh <rsourabh002@gmail.com>
2024-07-15 19:30:35 +0530

    Update DraftDetail.vue

--- src/views/DraftDetail.vue
+++ src/views/DraftDetail.vue
@@ -14,7 +14,7 @@
             <ion-item lines="none" class="ion-padding">
               <ion-label slot="start">
                 <h1 v-show="!isCountNameUpdating">{{ countName }}</h1>
-                <!-- Added class as we can't change the background of ion-input with css property, and we need to change the background to show the user that now value is editable -->
+                <!-- Added class as we can't change the background of ion-input with css property, and we need to change the background to show the user that now this value is editable -->                
                 <ion-input ref="countNameRef" :class="isCountNameUpdating ? 'name' : ''" v-show="isCountNameUpdating" aria-label="group name" v-model="countName"></ion-input>
                 <p>{{ currentCycleCount.countId }}</p>
               </ion-label>
