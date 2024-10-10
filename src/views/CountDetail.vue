62f58720abe177d63f71e18cc74dc370e2052829
amansinghbais <singh11amanbais@gmail.com>
2024-10-10 23:45:10 +0530

    Fixed: error showing in case of empty product while tab switch

--- src/views/CountDetail.vue
+++ src/views/CountDetail.vue
@@ -62,12 +62,12 @@
         </aside>
         <!--Product details-->
         <main class="main">
-          <div class="product" @scroll="onScroll">
+          <div class="product" @scroll="onScroll" v-if="filteredItems.length">
             <div class="image ion-padding-top" v-for="item in filteredItems" :key="item.importItemSeqId" :data-product-id="item.productId" :data-seq="item.importItemSeqId" :id="`${item.productId}-${item.importItemSeqId}`">
               <Image :src="getProduct(item.productId)?.mainImageUrl" />
             </div>
           </div>
-          <div class="detail">
+          <div class="detail" v-if="Object.keys(product)?.length">
             <ion-item lines="none">
               <ion-label class="ion-text-wrap">
                 <h1>{{ getProductIdentificationValue(productStoreSettings["productIdentificationPref"].primaryId, getProduct(product.productId)) }}</h1>
@@ -348,7 +348,7 @@ function updateFilteredItems() {
     store.dispatch("product/currentProduct", product.value);
     updateNavigationState(filteredItems.value.indexOf(product.value));
   } else {
-    store.dispatch("product/currentProduct", null);
+    store.dispatch("product/currentProduct", {});
     isFirstItem.value = true
     isLastItem.value = false
   }  
