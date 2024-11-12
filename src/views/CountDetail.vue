364fb1fc0a35f7b5b3522a1bd3f4787296bfe0c6
amansinghbais <singh11amanbais@gmail.com>
2024-11-12 19:26:05 +0530

    Fixed: reseting input count to empty string on bringing it to view for the first time (#494)

--- src/views/CountDetail.vue
+++ src/views/CountDetail.vue
@@ -423,6 +423,7 @@ async function scanProduct() {
         element.scrollIntoView({ behavior: 'smooth' });
       }
     }, 0);
+    if(!selectedItem.quantity && selectedItem.quantity !== 0) inputCount.value = ""
   } else if(selectedItem.itemStatusId === "INV_COUNT_CREATED") {
     if((!selectedItem.quantity && selectedItem.quantity !== 0) || product.value.isRecounting) {
       hasUnsavedChanges.value = true;
