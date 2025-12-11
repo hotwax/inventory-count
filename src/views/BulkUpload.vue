ff19ddf153bdfa73295e1bd58a8a015fd45bf360
Chinmay Sharma <152295063+Chinmay2107@users.noreply.github.com>
2025-12-11 11:01:31 +0530

    Simplify next execution timestamp assignment

--- src/views/BulkUpload.vue
+++ src/views/BulkUpload.vue
@@ -191,9 +191,7 @@ async function fetchJobExecutionTime() {
   );
 
   if (!hasError(jobResp) && jobResp.data?.jobDetail?.nextExecutionDateTime) {
-    nextExecutionTimestamp.value = DateTime.fromMillis(
-      jobResp.data.jobDetail.nextExecutionDateTime
-    ).toMillis();
+    nextExecutionTimestamp.value = jobResp.data.jobDetail.nextExecutionDateTime;
   }
 }
 
