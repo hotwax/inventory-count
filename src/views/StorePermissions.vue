a1c5bfb099ee377019266ed5869d35663f3299b1
Aditya Patel <heyadityapatel@gmail.com>
2025-12-11 12:47:07 +0530

    feat: Add "Preview count" permission card with security group management and update grid layout for permission cards.

--- src/views/StorePermissions.vue
+++ src/views/StorePermissions.vue
@@ -34,6 +34,54 @@
             </ion-toggle>
           </ion-item>
         </ion-card>
+        <ion-card>
+          <ion-card-header>
+            <ion-card-title>
+              {{ translate('Preview count') }}
+            </ion-card-title>
+          </ion-card-header>
+          <ion-card-content>
+            <p>{{ translate("Select security groups that can preview products in a directed count before the start time.") }}</p>
+          </ion-card-content>
+          <ion-list>
+            <ion-item-divider color="light">
+              {{ translate('Security groups') }}
+              <ion-button slot="end" fill="clear" size="small">
+                {{ translate('Add') }}
+                <ion-icon slot="end" :icon="addCircleOutline"></ion-icon>
+              </ion-button>
+            </ion-item-divider>
+            <ion-button fill="outline" expand="block" class="ion-margin">
+              <ion-icon slot="start" :icon="addOutline"></ion-icon>
+              {{ translate('Add security group') }}
+            </ion-button>
+            <ion-item button>
+              <ion-label>{{ translate('View history') }}</ion-label>
+              <ion-icon slot="end" :icon="timeOutline"></ion-icon>
+            </ion-item>
+            <ion-item>
+              <ion-label>Security group description</ion-label>
+              <ion-button color="medium" fill="clear" slot="end" id="security-group-popover-trigger">
+                <ion-icon slot="icon-only" :icon="ellipsisVerticalOutline"></ion-icon>
+              </ion-button>
+            </ion-item>
+            <ion-popover trigger="security-group-popover-trigger" :dismiss-on-select="true">
+              <ion-content>
+                <ion-list>
+                  <ion-item button :detail="false">
+                    <ion-label>
+                      Date added
+                      <p>added to group</p>
+                    </ion-label>
+                  </ion-item>
+                  <ion-item button :detail="false">
+                    <ion-label>{{ translate('Remove') }}</ion-label>
+                  </ion-item>
+                </ion-list>
+              </ion-content>
+            </ion-popover>
+          </ion-list>
+        </ion-card>
       </div>
     </ion-content> 
   </ion-page>
@@ -45,10 +93,14 @@ import {
   IonCardContent,
   IonCardHeader,
   IonCardTitle,
+  IonButton,
   IonContent,
   IonHeader,
   IonItem,
+  IonItemDivider,
+  IonList,
   IonPage,
+  IonPopover,
   IonTitle,
   IonToggle,
   IonToolbar,
@@ -58,7 +110,13 @@ import {
 } from "@ionic/vue";
 import { translate } from '@/i18n'
 import { useProductStore } from "@/stores/productStore";
-import { storefrontOutline } from "ionicons/icons";
+import {
+  storefrontOutline,
+  addCircleOutline,
+  addOutline,
+  timeOutline,
+  ellipsisVerticalOutline
+} from "ionicons/icons";
 
 onIonViewWillEnter(async () => {
   await useProductStore().getSettings(useProductStore().getCurrentProductStore.productStoreId)
@@ -73,7 +131,7 @@ function updateProductStoreSetting(event: any, key: string) {
 <style scoped>
 .permission-cards {
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(325px, 3fr));
+  grid-template-columns: repeat(auto-fill, minmax(325px, 1fr));
   align-items: start;
 }
 </style>
